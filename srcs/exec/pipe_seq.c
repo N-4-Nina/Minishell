9b225b40fb1fd8c043ca5ae5728805b418649834
Nina <abouchaud@student.42.fr>
2021-06-16 07:57:30 +0200

    just in case

--- srcs/exec/pipe_seq.c
+++ srcs/exec/pipe_seq.c
@@ -1,5 +1,15 @@
 #include "../../includes/nsh.h"
 
+void    do_fork(t_sh *nsh, pid_t *pid)
+{
+    *pid = fork();
+    if (*pid == -1)
+    {
+        //set status
+        nsh_exit(nsh, void);
+    }
+}
+
 int exec_pipe_seq(t_sh *nsh, t_cmd *cmd)
 {
     int     i;
@@ -7,11 +17,14 @@ int exec_pipe_seq(t_sh *nsh, t_cmd *cmd)
     int     origin[2];
 
     i = 0;
-    // while (i < cmd->smpnb)
-    // {
+    origin[0] = 0;
+    origin[1] = 1;
 
-    //     i++;
-    // }
+    while (i < cmd->smpnb)
+    {
+
+        i++;
+    }
     (void)nsh;
     (void)cmd;
     return (0);
